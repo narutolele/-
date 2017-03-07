1f52112c3628b9eb65629ed488dc4a0144a1bc32
narutolele <553419010@qq.com>
2017-03-07 10:28:18 +0800

    这是我的第一个比较完整的项目，有服务器文件，用nodejs

---
+++ sever.js
@@ -0,0 +1,36 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 4425;
+
+var server = http.createServer(function (request, response) {
+
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
+
+  //从这里开始看，上面不要看
+
+  if (path === '/') {
+    var string = fs.readFileSync('./index.html')
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    response.end(string)
+  } if (path === '/style.css') {
+    var string = fs.readFileSync('./style.css')
+    response.setHeader('Content-Type','text/css')
+    response.end(string)
+  } if (path === '/main.js') {
+    var string = fs.readFileSync('./main.js')
+    response.setHeader('Content-Type','application/javacsript')
+    response.end(string)
+  } else {
+    response.end('404')
+  }
+
+  // 代码结束，下面不要看
+})
+
+server.listen(port)
+console.log('监听 4425 成功')
